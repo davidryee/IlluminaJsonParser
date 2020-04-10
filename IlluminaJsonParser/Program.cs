cc5c1f88a2ebc7cab69357a71be79c292a94c7cd
David Yee <davidryee@gmail.com>
2020-04-09 17:02:15 -0700

    Initial Commit

---
+++ IlluminaJsonParser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IlluminaJsonParser
+{
+    public class IlluminaJsonParser
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the path to the file you wish to use as input. File must be in valid json format.");
+            var filePath = Console.ReadLine();
+        }
+    }
+}
